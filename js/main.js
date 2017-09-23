d215a12858649ac6b4c8289a7e092cf4a477016b
Miaababikir <miaababikir@gmail.com>
2017-09-23 11:45:43 +0300

    Uplaod Project Files

---
+++ js/main.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+
+      // Nice Scroll
+      $("html").niceScroll();
+
+    $(function() {
+
+      'use strict';
+
+      $(window).on("scroll", function() {
+        window.console.log($(".navbar").height());
+        window.console.log($(window).scrollTop());
+
+        var navbar = $(".navbar");
+        if ($(window).scrollTop() >= navbar.height()) {
+          if (!$('#nav').hasClass('scrolled')) {
+            navbar.addClass('scrolled');
+          }
+        } else {
+          navbar.removeClass('scrolled');
+        }
+
+      });
+
+    });
+
+});
