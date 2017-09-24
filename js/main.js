7ad8aa1da5e1876898f18d30513e25f95ef35610
Miaababikir <miaababikir@gmail.com>
2017-09-24 19:11:33 +0300

    Finishing up pages

--- js/main.js
+++ js/main.js
@@ -25,3 +25,35 @@ $(document).ready(function() {
     });
 
 });
+
+// Loading Screen
+$( window ).on('load', function()
+{
+  $(".loading-overlay .sk-folding-cube").delay(4000).fadeOut(1000, function()
+  {
+    $('body').css('overflow', 'auto');
+    $(this).parent().fadeOut(1000,function()
+    {
+      $(this).remove();
+    });
+  });
+});
+
+$('#type').typeIt({
+     strings: 'This is a simple string.',
+     speed: 50,
+     autoStart: false
+});
+
+$('#type1').typeIt({
+     strings: 'This is a simple string.',
+     speed: 50,
+     autoStart: false
+});
+
+$('#weAreTheBest').typeIt({
+     strings: 'of our graduates got jobs related to the degree they received at our university.',
+     speed: 100,
+     startDelay: 1000,
+     autoStart: false
+});
